a6a4136194c1df64347bf864e1198b20392cb858
Andrea Estrada Macias <andrea.estradam21@gmail.com>
2020-03-03 13:10:43 -0600

    Laboratorio 2 DS1 avance 2

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -39,9 +39,15 @@ namespace MVCLaboratorio.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Edit(int idVideo)
+        public ActionResult Edit(int idVideo, string titulo, int repro, string url)
         {
-            return View();
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@repro", repro));
+            parametros.Add(new SqlParameter("@url", url));
+            BaseHelper.ejecutarSentencia("sp_video_modificar", CommandType.StoredProcedure, parametros);
+            return RedirectToAction("Index", "Video");
         }
         public ActionResult Delete()
         {
